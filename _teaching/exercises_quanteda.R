fe030c120912fc4a58dc67b2f7c808a0b330cef5
phimeyer <p.meyer@ipw.uni-hannover.de>
2020-12-17 19:18:16 +0100

    Add files via upload

---
+++ _teaching/exercises_quanteda.R
@@ -0,0 +1,20 @@
+# Aufgaben und Übungen zum Umgang mit quanteda
+
+
+# Aufgabe: Arbeiten mit quanteda
+
+# Die Aufgaben in dieser Woche sind etwas anders geartet. Ich möchte hier die Möglichkeit nutzen
+# um euch eine wichtige Datenquelle (Comparative Manifesto Project Daten) UND den Umgang mit quanteda näherzubringen. 
+# Bitte lest euch die Beschreibungen auf den folgenden zwei Internetseiten GUT durch. 
+# Kopiert dann anschließend die dort gezeigten Codezeilen, versucht sie mit eurem bisherigen Wissen zu verstehen
+# und führt sie letztlich bei euch auch aus. 
+# Ich hoffe, dass Ihr die gleichen Ergebnisse, wie auf den Seiten angezeigt, produziert.
+
+# Letztlich ist diese Aufgabe vor allem dazu da, dass ihr mal etwas rumspielt, Fehlermeldungen produziert und die Manifesto-Daten 
+# (https://manifesto-project.wzb.eu/) näher kennenlernt!
+
+# Hier sind die zwei Internetseiten: 
+# 1. https://manifesto-project.wzb.eu/tutorials/firststepsmanifestoR
+# 2. https://manifesto-project.wzb.eu/down/tutorials/quanteda.html
+
+
