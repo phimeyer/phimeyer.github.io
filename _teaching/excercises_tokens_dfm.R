1d474e7ed670d350195e899c6e55cdae03ef90cb
phimeyer <p.meyer@ipw.uni-hannover.de>
2021-02-18 17:16:58 +0100

    Add files via upload

---
+++ _teaching/excercises_tokens_dfm.R
@@ -0,0 +1,21 @@
+# Aufgaben und Übungen zum Umgang mit quanteda
+
+# Tokens und DFMs
+
+# Aufgabe 1
+
+# Repliziere die tokens und die DFM aus dem Tutorialdokument
+
+# Aufgabe 2
+
+# Inspiziere die DFM identifiziere unnötige bzw. uninteressante Wörter (z.B. Städtenamen), Abkürzungen oder vergessene Zahlen. 
+# Nutze dafür sowohl die Befehle aus dem Tutorial, als auch andere Wege und Werkzeuge (z.B. gefunden durch eine Internetsuche)
+
+# Aufgabe 3 
+
+# Erstelle eine Liste deiner gefundenen unnötigen bzw. uninteressanten Wörter und wende sie auf die token-List und 
+# die DFM an. 
+
+# Aufgabe 4 
+
+# Repliziere die Visualisierungen aus dem Tutorialdokument mit deiner neuen DFM. 
