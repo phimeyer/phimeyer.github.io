de7e888a37df4dfe6eff13cca185ae3be4741a3c
phimeyer <p.meyer@ipw.uni-hannover.de>
2021-02-19 15:42:24 +0100

    Add files via upload

---
+++ _teaching/R_excercises_dictionary.R
@@ -0,0 +1,31 @@
+# Aufgaben: Textanalyse mit Wörterbüchern und Sentiments mit `quanteda`
+
+
+# Ein Wörterbuch ist im Grunde eine Funktion, die Wörtern Werte zuweist. Ein 
+# typisches Wörterbuch weist jeder Erwähnung eines Wortes eine Eins (1) zu. 
+# Alle Wörter die nicht im Wörterbuch vertreten sind, wird der Wert Null (0) zugewiesen. 
+# Es muss jedoch nicht so restriktiv sein! Diese Entscheidung muss immer 
+# theoriegeleitet und kontextabhängig erfolgen. 
+
+# Aufgabe 1. Ladet den Datensatz zu Entscheidungen des Bundesverfassungsgericht (bekannt
+# den vorherigen Tutorials) und generiet eine DTM. 
+
+# Aufgabe 2. Inspiziert die DTM mittels einer Word cloud oder indem ihre euch die 
+# am häufigsten vorkommenden Wörter anschaut (auch auch mit alternativen Wegen, 
+# die natürlich im Internet findet).
+
+# Aufgabe 3a. Definiert mindestens zwei Wörterbuchlategorien und ordnet diesen Kategorien 
+# passende Wörter zu. 
+
+##### ODER ######
+
+# Aufgabe 3b. Such ein speziell für die deutsche Spache entwickeltes Wörterbuch 
+# und lest es in R ein (ihr findet ihr mittels Internetsuche sehr gute csv.Dateien
+# mit Wörterbüchern zur deutschen Sprache). 
+
+# Aufgabe 4. Wendet euer eigenes Wörterbuch (Aufgabe 3a) oder das von euch 
+# gefundete Wörterbuch (Aufgabe 3b) auf eure Gerichtsentscheidungs-DTM an (nutzt 
+# dafür den präsentierten Code aus dem Tutorial).
+
+# Aufgabe 5. Validiert eure Ergebnisse und optimiert euer Wörterbuch (nutzt dafür 
+# den präsentierten Code aus dem Tutorial).
