57f729917e1bc03ce7dd71952557af4de91d60e4
phimeyer <p.meyer@ipw.uni-hannover.de>
2021-03-10 11:34:58 +0100

    Add files via upload

--- _teaching/R_excercises_dictionary.R
+++ _teaching/R_excercises_dictionary.R
@@ -1,27 +1,27 @@
 # Aufgaben: Textanalyse mit Wörterbüchern und Sentiments mit `quanteda`
 
-
-# Ein Wörterbuch ist im Grunde eine Funktion, die Wörtern Werte zuweist. Ein 
+# Einfach gesagt weist man mit einem Wörterbuch Wörtern definierte Werte zu. Ein 
 # typisches Wörterbuch weist jeder Erwähnung eines Wortes eine Eins (1) zu. 
 # Alle Wörter die nicht im Wörterbuch vertreten sind, wird der Wert Null (0) zugewiesen. 
 # Es muss jedoch nicht so restriktiv sein! Diese Entscheidung muss immer 
 # theoriegeleitet und kontextabhängig erfolgen. 
 
 # Aufgabe 1. Ladet den Datensatz zu Entscheidungen des Bundesverfassungsgericht (bekannt
-# den vorherigen Tutorials) und generiet eine DTM. 
+# den vorherigen Tutorials) und erstellt eine DTM. 
 
 # Aufgabe 2. Inspiziert die DTM mittels einer Word cloud oder indem ihre euch die 
-# am häufigsten vorkommenden Wörter anschaut (auch auch mit alternativen Wegen, 
-# die natürlich im Internet findet).
+# am häufigsten vorkommenden Wörter anschaut (auch gerne mit alternativen Wegen, 
+# die ihr natürlich im Internet findet).
 
-# Aufgabe 3a. Definiert mindestens zwei Wörterbuchlategorien und ordnet diesen Kategorien 
+# Aufgabe 3a. Definiert mindestens zwei Wörterbuchkategorien und ordnet diesen Kategorien 
 # passende Wörter zu. 
 
 ##### ODER ######
 
-# Aufgabe 3b. Such ein speziell für die deutsche Spache entwickeltes Wörterbuch 
+# Aufgabe 3b. Sucht ein speziell für die deutsche Spache entwickeltes Wörterbuch 
 # und lest es in R ein (ihr findet ihr mittels Internetsuche sehr gute csv.Dateien
-# mit Wörterbüchern zur deutschen Sprache). 
+# mit Wörterbüchern zur deutschen Sprache. Hinweis: im Tutorial wird auch eines 
+# genannt). 
 
 # Aufgabe 4. Wendet euer eigenes Wörterbuch (Aufgabe 3a) oder das von euch 
 # gefundete Wörterbuch (Aufgabe 3b) auf eure Gerichtsentscheidungs-DTM an (nutzt 
