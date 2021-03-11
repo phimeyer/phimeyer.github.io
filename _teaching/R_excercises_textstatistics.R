35cbf89aaa44c8ee4bcfb76d070665360954348c
phimeyer <p.meyer@ipw.uni-hannover.de>
2021-03-11 15:49:23 +0100

    Add files via upload

---
+++ _teaching/R_excercises_textstatistics.R
@@ -0,0 +1,19 @@
+# Aufgaben: Textstatistiken und Textähnlichkeiten
+
+
+# Aufgabe 1: Erstellt einen Korpus und danach eine DTM/DFM nur mit den Reden von Trump und Obama
+
+# Aufgabe 2: Vergleiche die Keynesswerte der Rede von Trump (target) mit der von Obama. 
+
+# Aufgabe 3: Identifiziere die Wörterpaar die am häufigsten gemeinsam auftreten. 
+
+# Aufgabe 4: Lass dir die KWIC für "citizens" und "my fellow americans" ausgeben 
+# und visualisiere anschließend die Positionen dieser Begriffe/Satzstücke in den Reden der US Präsidenten 
+
+# Aufgabe 5: Berechne die Textähnlichkeit der Reden von Obama und Trump. 
+# Verwende dafür die Jaccard Similarity
+
+# Aufgabe 6: Berechne die Textdiversität und die Lesbarkeit aller Antrittsreden 
+# der US Präsidenten. Visualisiere deine Ergebnisse
+
+
